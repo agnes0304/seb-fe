b08337c1969b94d67a2d0763986973d283731b69
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-16 12:50:46 +0900

    fs module file no.03 ongoing

--- fe-sprint-async-and-promise-main/part-2/03_basicChaining.js
+++ fe-sprint-async-and-promise-main/part-2/03_basicChaining.js
@@ -1,19 +1,33 @@
-const path = require('path');
-const { getDataFromFilePromise } = require('./02_promiseConstructor');
+const path = require("path");
+const { getDataFromFilePromise } = require("./02_promiseConstructor");
 
-const user1Path = path.join(__dirname, 'files/user1.json');
-const user2Path = path.join(__dirname, 'files/user2.json');
+const user1Path = path.join(__dirname, "files/user1.json");
+const user2Path = path.join(__dirname, "files/user2.json");
 
-// HINT: getDataFromFilePromise(user1Path) 및 getDataFromFilePromise(user2Path)를 이용해 작성합니다
+// HINT: getDataFromFilePromise(user1Path) 및
+// getDataFromFilePromise(user2Path)를 이용해 작성합니다
 const readAllUsersChaining = () => {
-  // TODO: 여러개의 Promise를 then으로 연결하여 작성합니다
-}
+  // fs모듈 직접 사용금지
+  // json파일 불러오고 두 파일을 합쳐서 최종적으로 두 객체가 담긴 array를 생성.
+  return getDataFromFilePromise(user1Path)
+    .then((u1json) => {
+      // 클로저 활용 -> 내부 함수에서 사용 가능.
+      return getDataFromFilePromise(user2Path).then((u2json) => {
+        // return `[${u1json}, ${u2json}]`;
+        return '['+u1json+','+u2json+']';
+      });
+    })
+    .then((arr) => JSON.parse(arr));
+  // const u1json = getDataFromFilePromise(user1Path);
+  // const u2json = getDataFromFilePromise(user2Path);
+  // u1json u2json이 합쳐져야 함. ,로 ','
+};
 
-// readAllUsersChaining();
+readAllUsersChaining();
 
 module.exports = {
-  readAllUsersChaining
-}
+  readAllUsersChaining,
+};
 
 // user1.json content
 // {
