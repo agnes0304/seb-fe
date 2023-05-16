ae920104f16cda9fab1b9c503bcefb84a8875e6c
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-16 11:12:12 +0900

    understanding async ongoing

--- fe-sprint-async-and-promise-main/part-1/02_promiseConstructor.js
+++ fe-sprint-async-and-promise-main/part-1/02_promiseConstructor.js
@@ -1,5 +1,22 @@
+// const sleep = (wait) => {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, wait);
+//   });
+// }
+
 const sleep = (wait) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, wait);
+    setTimeout(() => {
+      resolve('hello');
+    }, wait);
   });
-}
+};
+
+// catch test
+// const sleep = (wait) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       reject(new Error('에러'));
+//     }, wait);
+//   });
+// };
