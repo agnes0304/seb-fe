f00d451cfcc07e0ff1ecc0c7b2b50911728fbcda
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-16 14:21:40 +0900

    fs module file no.05 done

--- fe-sprint-async-and-promise-main/part-2/05_asyncAwait.js
+++ fe-sprint-async-and-promise-main/part-2/05_asyncAwait.js
@@ -1,15 +1,23 @@
-const path = require('path');
-const { getDataFromFilePromise } = require('./02_promiseConstructor');
+const path = require("path");
+const { getDataFromFilePromise } = require("./02_promiseConstructor");
 
-const user1Path = path.join(__dirname, 'files/user1.json');
-const user2Path = path.join(__dirname, 'files/user2.json');
+const user1Path = path.join(__dirname, "files/user1.json");
+const user2Path = path.join(__dirname, "files/user2.json");
 
-const readAllUsersAsyncAwait = () => {
-  // TODO: async/await 키워드를 이용해 작성합니다
-}
+const readAllUsersAsyncAwait = async () => {
+  const u1 = await getDataFromFilePromise(user1Path);
+  const u2 = await getDataFromFilePromise(user2Path);
+  const result = `[${u1}, ${u2}]`;
+  const jsonResult = JSON.parse(result);
+  return jsonResult;
 
-// readAllUsersAsyncAwait();
+};
+
+readAllUsersAsyncAwait();
 
 module.exports = {
-  readAllUsersAsyncAwait
-}
+  readAllUsersAsyncAwait,
+};
+
+// readAllUsersChaining, readAllUsers와 같은 결과를 리턴합니다.
+// 이번에는 async 및 await 키워드를 사용해서 해결해야 합니다.
