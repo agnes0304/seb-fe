7bc5396576ee2fc19dddfc4dfc5a912c0d666890
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-17 12:57:43 +0900

    fetch api file no.01 ongoing

--- fe-sprint-async-and-promise-main/part-3/01_basicChaining.js
+++ fe-sprint-async-and-promise-main/part-3/01_basicChaining.js
@@ -1,13 +1,26 @@
-const newsURL = 'http://localhost:4999/data/latestNews';
-const weatherURL = 'http://localhost:4999/data/weather';
+const newsURL = "http://localhost:4999/data/latestNews";
+const weatherURL = "http://localhost:4999/data/weather";
 
 function getNewsAndWeather() {
   // TODO: fetch을 이용해 작성합니다
-  // TODO: 여러개의 Promise를 then으로 연결하여 작성합니다
+  // TODO: 여러개의 Promise를 then으로 연결하여 작성합니다.
+  let result = {};
+  return fetch(newsURL)
+    .then((response) => response.json())
+    .then((newsData) => {
+      result.news = newsData.data;
+      return fetch(weatherURL)})
+        .then((response) => response.json())
+        .then((weatherData) => {
+          result.weather = weatherData;
+          return result;
+        });
+    };
+
+if (typeof window === "undefined") {
+  module.exports = {
+    getNewsAndWeather,
+  };
 }
 
-if (typeof window === 'undefined') {
-  module.exports = {
-    getNewsAndWeather
-  }
-}
+// getNewsAndWeather();
